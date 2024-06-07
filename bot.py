f17b7438649a0f64a442b50efc49082bcf0bb02e
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-06-06 22:10:19 -0400

    Add myLobbies_autocomplete

--- bot.py
+++ bot.py
@@ -23,6 +23,10 @@ async def job_autocomplete(ctx: discord.AutocompleteContext):
     jobs = db.jobs.find({'guild_id': ctx.interaction.guild.id})
     return [job['name'] for job in jobs if 'name' in job and len(job['name']) <= 50]
 
+async def myLobbies_autocomplete(ctx: discord.AutocompleteContext):
+    lobbies = db.lobbies.find({'guild_id': ctx.interaction.guild.id})
+    return [lobby['name'] for lobby in lobbies if 'name' in lobby and len(lobby['name']) <= 50]
+
 @bot.slash_command(name="profile", description="Setup your profile")
 async def profile(ctx):
     await ctx.send_modal(ProfileModal())
